09bd761ce34ce9de67057b682ca27b3966ffccf0
Salemah <kadertanvir1@gmail.com>
2022-04-08 16:08:42 +0600

    login controlerr updated

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,15 +6,19 @@ use App\Models\Users;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
-class LoginController extends Controller {
+class LoginController extends Controller
+{
 
-    public function index() {
-        return view( 'login' );
+    public function index()
+    {
+        return view('login');
     }
 
-    public function verify( Request $req ) {
+    public function verify(Request $req)
+    {
         $validator = Validator::make(
-            $req->all(), [
+            $req->all(),
+            [
                 'email' => 'required|email',
                 'password' => 'required|min:6',
 
@@ -30,66 +34,68 @@ class LoginController extends Controller {
             return response()->json([
                 'validation_errors' => $validator->errors(),
             ]);
-        }
+        } else {
 
-        else
-    {
+            $result = Users::where('email', $req->email)
+                ->where('password', $req->password)->first();
+            if ($result) {
+                if ($result->type == "patient") {
+                    return response()->json([
+                        'status' => 'success',
+                        'message' => 'Login Successfully',
+                        'id' => $result->id,
+                        'name' => $result->name,
+                        'email' => $result->email,
+                        'type' => $result->type,
+                        'username' => $result->username,
 
-        $result = Users::where('email',$req->email)
-        ->where('password',$req->password)->first();
-        if($result){
-            if ( $result->type == "patient" )
-            {
+
+                    ]);
+                }
+            } else {
                 return response()->json([
-                    'success' => "bgfbfb",
+                    'status' => 'notFound',
+                    'message' => 'User not Found',
                 ]);
             }
+
+
+
+            //    foreach ( $result as $res ) {
+            //         $username = $res->username;
+            //         $email = $res->email;
+            //         $name = $res->name;
+            //          $type = $res->type;
+            //          $id = $res->id;
+            //         }
+            //     if ( count( $result ) > 0 ) {
+            //         if ( $type == "patient" )
+            //         {
+            //                     return response()->json([
+            //                     'success' => $username,
+            //                 ]);
+
+            //         }
+
+            //         else {
+            //             return response()->json([
+            //                 'role' => 'doesnot match',
+            //                 'message' => 'User not Found'
+            //             ]);
+            //     }
+            // }
+            //     else if( !$email ==''  ) {
+            //         return response()->json([
+            //             'status' => 'notFound',
+            //             'message' => 'User not Found'
+            //         ]);
+
+
+            // }
         }
-        else{
-            return response()->json([
-                'validation_errors' => "password & email mismatced",
-            ]);
-        }
-
-
-
-    //    foreach ( $result as $res ) {
-    //         $username = $res->username;
-    //         $email = $res->email;
-    //         $name = $res->name;
-    //          $type = $res->type;
-    //          $id = $res->id;
-    //         }
-    //     if ( count( $result ) > 0 ) {
-    //         if ( $type == "patient" )
-    //         {
-    //                     return response()->json([
-    //                     'success' => $username,
-    //                 ]);
-
-    //         }
-
-    //         else {
-    //             return response()->json([
-    //                 'role' => 'doesnot match',
-    //                 'message' => 'User not Found'
-    //             ]);
-    //     }
-    // }
-    //     else if( !$email ==''  ) {
-    //         return response()->json([
-    //             'status' => 'notFound',
-    //             'message' => 'User not Found'
-    //         ]);
-
-
-    // }
+    }
 }
-    }}
 
 
 
     //
-
-
-
